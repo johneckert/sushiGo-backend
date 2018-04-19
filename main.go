c6a1f0fec2fc546f15939225678e913943df8258
johneckert <johnteckert@gmail.com>
2018-04-18 22:49:31 -0400

    built show function

--- main.go
+++ main.go
@@ -13,11 +13,10 @@ import (
 
 //Roll is model for sushi
 type Roll struct {
-	ID           string `json:"id"`
-	ImageNumber  string `json:"imageNumber"`
-	Name         string `json:"name"`
-	Ingredients  string `json:"ingredients"`
-	Instructions string `json:"instructions"`
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
 }
 
 //Init rolls var as a slice
@@ -31,11 +30,22 @@ func getRolls(w http.ResponseWriter, r *http.Request) {
 
 //Get Single Roll
 func getRoll(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range rolls {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Roll{})
 }
 
 //Create a New Roll
 func createRoll(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Content-Type", "application/json")
+	// var roll Roll
+	// _ =
 
 }
 
@@ -54,29 +64,29 @@ func main() {
 	router := mux.NewRouter()
 
 	//stump data TODO: Implement database
-	rolls = append(rolls, Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Tuna, Chili sauce, Nori, Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Tuna, Chili sauce, Nori, Rice"})
 
-	rolls = append(rolls, Roll{ID: "2", ImageNumber: "4", Name: "California Roll", Ingredients: "Crab, Cucumber, Avocado, Nori, Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "2", ImageNumber: "4", Name: "California Roll", Ingredients: "Crab, Cucumber, Avocado, Nori, Rice"})
 
-	rolls = append(rolls, Roll{ID: "3", ImageNumber: "10", Name: "Caterpillar Roll", Ingredients: "Tempura Shrimp, Cucumber, Avocado, Nori, Rice, Eel Sauce", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "3", ImageNumber: "10", Name: "Caterpillar Roll", Ingredients: "Tempura Shrimp, Cucumber, Avocado, Nori, Rice, Eel Sauce"})
 
-	rolls = append(rolls, Roll{ID: "4", ImageNumber: "7", Name: "Spider Roll", Ingredients: "Tempura Crab, Nori, Rice, Avocado, Spicy mayonnaise", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "4", ImageNumber: "7", Name: "Spider Roll", Ingredients: "Tempura Crab, Nori, Rice, Avocado, Spicy mayonnaise"})
 
-	rolls = append(rolls, Roll{ID: "5", ImageNumber: "2", Name: "Unagi Roll", Ingredients: "Broiled Eel, Avocado, Nori, Rice, Eel sauce", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "5", ImageNumber: "2", Name: "Unagi Roll", Ingredients: "Broiled Eel, Avocado, Nori, Rice, Eel sauce"})
 
-	rolls = append(rolls, Roll{ID: "6", ImageNumber: "1", Name: "Philidelphia Roll", Ingredients: "Smoked Salmon, Cream Cheese, Cucumber, Nori, Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "6", ImageNumber: "1", Name: "Philidelphia Roll", Ingredients: "Smoked Salmon, Cream Cheese, Cucumber, Nori, Rice"})
 
-	rolls = append(rolls, Roll{ID: "7", ImageNumber: "3", Name: "Salmon Roll", Ingredients: "Salmon, Nori, Rice, Tobiko", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "7", ImageNumber: "3", Name: "Salmon Roll", Ingredients: "Salmon, Nori, Rice, Tobiko"})
 
-	rolls = append(rolls, Roll{ID: "8", ImageNumber: "6", Name: "Avocado Roll", Ingredients: "Avocado, Nori, Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "8", ImageNumber: "6", Name: "Avocado Roll", Ingredients: "Avocado, Nori, Rice"})
 
-	rolls = append(rolls, Roll{ID: "9", ImageNumber: "9", Name: "Rainbow Roll", Ingredients: "Yellow Tail, Salmon, Cucumber, Nori, Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "9", ImageNumber: "9", Name: "Rainbow Roll", Ingredients: "Yellow Tail, Salmon, Cucumber, Nori, Rice"})
 
-	rolls = append(rolls, Roll{ID: "10", ImageNumber: "5", Name: "Tobiko Roll", Ingredients: "Tobiko, Nori, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "10", ImageNumber: "5", Name: "Tobiko Roll", Ingredients: "Tobiko, Nori, Sushi Rice"})
 
-	rolls = append(rolls, Roll{ID: "11", ImageNumber: "11", Name: "Yellow Tail Sushi", Ingredients: "Yellow Tail, Wasabi, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "11", ImageNumber: "11", Name: "Yellow Tail Sushi", Ingredients: "Yellow Tail, Wasabi, Sushi Rice"})
 
-	rolls = append(rolls, Roll{ID: "12", ImageNumber: "1", Name: "Salmon Sushi", Ingredients: "Salmon, Wasabi, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "12", ImageNumber: "1", Name: "Salmon Sushi", Ingredients: "Salmon, Wasabi, Sushi Rice"})
 
 	//endpoints
 	router.HandleFunc("/sushi", getRolls).Methods("GET")
