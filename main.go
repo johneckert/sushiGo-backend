0d2ddf4082e19da41a799fccc944a5946a15c2c1
johneckert <johnteckert@gmail.com>
2018-04-18 22:21:38 -0400

    updated recipes

--- main.go
+++ main.go
@@ -54,25 +54,25 @@ func main() {
 	router := mux.NewRouter()
 
 	//stump data TODO: Implement database
-	rolls = append(rolls, Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Big Eye Tuna, Chili sauce, Nori, Sushi Rice, Shichimi Togarashi, Tobiko (optional), Sesame oil (optional)", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Tuna, Chili sauce, Nori, Rice", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "2", ImageNumber: "4", Name: "California Roll", Ingredients: "Crab, Sliced Cucumber, Avocado, Nori, Sushi Rice, Tobiko (optional)", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "2", ImageNumber: "4", Name: "California Roll", Ingredients: "Crab, Cucumber, Avocado, Nori, Rice", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "3", ImageNumber: "10", Name: "Caterpillar Roll", Ingredients: "4 pieces of Tempura Shrimp, Sliced Cucumber, Avocado, Nori, Sushi Rice, Eel Sauce, Sesame seeds", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "3", ImageNumber: "10", Name: "Caterpillar Roll", Ingredients: "Tempura Shrimp, Cucumber, Avocado, Nori, Rice, Eel Sauce", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "4", ImageNumber: "7", Name: "Spider Roll", Ingredients: "Tempura Soft-shell Crab, Nori, Sushi Rice, Avocado, Spicy mayonnaise (optional)", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "4", ImageNumber: "7", Name: "Spider Roll", Ingredients: "Tempura Crab, Nori, Rice, Avocado, Spicy mayonnaise", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "5", ImageNumber: "2", Name: "Unagi Roll", Ingredients: "Broiled Eel, Avocado, Nori, Sushi Rice, Eel sauce", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "5", ImageNumber: "2", Name: "Unagi Roll", Ingredients: "Broiled Eel, Avocado, Nori, Rice, Eel sauce", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "6", ImageNumber: "1", Name: "Philidelphia Roll", Ingredients: "Smoked Salmon, Cream Cheese, Cucumber, Nori, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "6", ImageNumber: "1", Name: "Philidelphia Roll", Ingredients: "Smoked Salmon, Cream Cheese, Cucumber, Nori, Rice", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "7", ImageNumber: "3", Name: "Salmon Roll", Ingredients: "Salmon, Nori, Sushi Rice, Tobiko (optional)", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "7", ImageNumber: "3", Name: "Salmon Roll", Ingredients: "Salmon, Nori, Rice, Tobiko", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "8", ImageNumber: "6", Name: "Avocado Roll", Ingredients: "Avocado, Nori, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "8", ImageNumber: "6", Name: "Avocado Roll", Ingredients: "Avocado, Nori, Rice", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "9", ImageNumber: "5", Name: "Sweet Potato Roll", Ingredients: "Tempura Sweet Potato, Nori, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "9", ImageNumber: "9", Name: "Rainbow Roll", Ingredients: "Yellow Tail, Salmon, Cucumber, Nori, Rice", Instructions: "roll using mat"})
 
-	rolls = append(rolls, Roll{ID: "10", ImageNumber: "9", Name: "Sweet Potato Roll", Ingredients: "Tempura Sweet Potato, Nori, Sushi Rice", Instructions: "roll using mat"})
+	rolls = append(rolls, Roll{ID: "10", ImageNumber: "5", Name: "Tobiko Roll", Ingredients: "Tobiko, Nori, Sushi Rice", Instructions: "roll using mat"})
 
 	rolls = append(rolls, Roll{ID: "11", ImageNumber: "11", Name: "Yellow Tail Sushi", Ingredients: "Yellow Tail, Wasabi, Sushi Rice", Instructions: "roll using mat"})
 
